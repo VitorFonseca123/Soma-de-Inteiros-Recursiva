255d0b5e8f269c61455cc7133c0fec075de457c8
Vitor da Fonseca Rodrigues <vitorfon20@gmail.com>
2022-09-20 16:14:13 -0300

    commit
    resultado esperado = 3

--- SomaDeInteirosRecursivo.c
+++ SomaDeInteirosRecursivo.c
@@ -8,16 +8,17 @@ int soma(int v[], int tam)
         return 0;
     else
     {
-        if(v[tam - 1] < 0) return 0 + soma(v, tam - 1);
-        else return v[tam - 1] + soma(v, tam - 1);
-        
+        if (v[tam - 1] < 0)
+            return 0 + soma(v, tam - 1);
+        else
+            return v[tam - 1] + soma(v, tam - 1);
     }
 }
 
 int main()
 {
 
-    int vetor[5] = {-1, 0, 1, 2, 0};
+    int vetor[5] = {0, -1, 1, 0, 2};
     int n = 5;
 
     printf("Resultado = %d", soma(vetor, 5));
